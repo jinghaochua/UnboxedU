a6ee215108161111e58ce9eaf1bc6c2139fde83a
jinghaochua <jinghao_chua@hotmail.com>
2026-05-28 03:02:45 +0800

    added firebase setup connection

---
+++ app/lib/firebase.ts
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+//import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAaPZ50t7oc-NOUEKapwHfKWqwmfaaghjE",
+  authDomain: "unboxedu.firebaseapp.com",
+  projectId: "unboxedu",
+  storageBucket: "unboxedu.firebasestorage.app",
+  messagingSenderId: "153090787576",
+  appId: "1:153090787576:web:4cdabbd590731b2d02130d",
+  measurementId: "G-T9PVDZKRV8",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+//const analytics = getAnalytics(app);
